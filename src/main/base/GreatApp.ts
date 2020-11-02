89bd0c3752b8bc9b3f44ee987363e8706aa3fbe7
taurusdigitaldesign <taurusdigitaldesign@gmail.com>
2020-11-02 15:26:54 +0800

    feat: apply GreatApp and GreatWindow

--- src/main/base/GreatApp.ts
+++ src/main/base/GreatApp.ts
@@ -1,14 +1,29 @@
-import { app } from 'electron';
-
+import { app, BrowserWindow } from 'electron';
 class GreatApp {
-  static instance: any = app;
+  static instance: GreatApp;
+
+  app: any = app;
 
   constructor() {
+    if (!GreatApp.instance) {
+      GreatApp.instance = this;
+    }
     return GreatApp.instance;
   }
 
   static getInstance() {
     return GreatApp.instance;
+  }
+
+  on(event, callback) {
+    if (event === 'create') {
+      app.whenReady().then(callback);
+      app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) callback();
+      });
+      return;
+    }
+    app.on(event, callback);
   }
 }
 
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,31 +1,16 @@
 import { join } from 'path';
 import GreatApp from './base/GreatApp';
 import GreatWindow from './base/GreatWindow';
-import Net from './base/utils/Net';
 
-const app = GreatApp.getInstance();
+const great = new GreatApp();
 
-const createWindow = () => {
+great.on('create', () => {
   new GreatWindow({
     width: 800,
     height: 600
-  }).init(join(app.getAppPath(), './src/render/index.html'));
-};
-
-app.whenReady().then(createWindow);
-
-app.on('activate', function () {
-  if (GreatWindow.getAllWindows().length === 0) createWindow();
+  }).init(join(great.app.getAppPath(), './src/render/index.html'));
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
+great.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') great.app.quit();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-Net.ping('http://www.baidu.com');
